13cc8a27bcbfeda03810ed82d6dcbe185945488d
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-22 19:43:39 +0300

    d

---
+++ Example_to_symbol/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_to_symbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
+            StringBuilder temp = new StringBuilder(text);
+            char[] arr = text.ToCharArray();
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+
+                }
+            }
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
