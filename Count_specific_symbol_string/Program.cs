6ea829876729184e1fdd21f12b8ecb4a19acaf3b
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-25 18:53:44 +0300

    d

---
+++ Count_specific_symbol_string/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_specific_symbol_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
+            Console.Write("Enter symbol to count: ");
+            char symbol = Console.ReadKey().KeyChar;
+            int count = 0;           
+            foreach (char a in text)
+            {
+                if (symbol == a)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Symbol {symbol} appears in string {count} times");
+            Console.ReadKey();
+        }
+    }
+}
