9b4ae27c931a6a59b2b226d7462768a5304631e3
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-25 19:00:57 +0300

    j

---
+++ Paste_substring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paste_substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Enter substring to paste:");
+            string sub = Console.ReadLine();
+            Console.Write("Enter position (in number) where paste substring: ");
+            int position = int.Parse(Console.ReadLine());
+            text = text.Insert(position, sub);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
