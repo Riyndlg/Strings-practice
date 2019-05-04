f6016e68bb5926e63bebc7756bf4019b82f644b7
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-04 09:20:02 +0300

    xx

---
+++ Excessive white spaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excessive_white_spaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
+            text = text.Trim();
+            for (int i = 0; i < text.Length; i++)
+            {
+
+            }
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
