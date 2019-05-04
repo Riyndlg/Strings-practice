a67c497b5339cce1a57601f355c253bb13b04456
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-04 11:27:25 +0300

    x

--- Number for word/Program.cs
+++ Number for word/Program.cs
@@ -12,16 +12,23 @@ namespace Number_for_word
         {
             Console.WriteLine("Enter text:");
             string text = Console.ReadLine();
-            Console.WriteLine("Enter number of word: ");
+            Console.WriteLine("Enter number of word (start from 0): ");
             int number = int.Parse(Console.ReadLine());
             string [] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains(' '))
+                {
+
+                }
+            }
             if (number <= words.Length)
             {
                 for (int i = 0; i < words.Length; i++)
                 {
                     if (i == number)
                     {
-                        Console.WriteLine(words[i].Take(1));
+                        Console.WriteLine(words[i].First());
                     }
                 }
             }
