fdf5a9025d8ba85b4ba14ea8c561ca2556a3bc4f
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-04 16:17:47 +0300

    c

--- Function printer error/Program.cs
+++ Function printer error/Program.cs
@@ -10,6 +10,22 @@ namespace Function_printer_error
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter string with letters from a to z without spaces:");
+            string text = Console.ReadLine();
+            int illegal = 0;
+            string ill = "nopuqrstvwxyz";
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < ill.Length; j++)
+                {
+                    if (text[i] == ill[j])
+                    {
+                        illegal++;
+                    }
+                }
+            }
+            Console.WriteLine(illegal / text.Length);
+            Console.ReadKey();
         }
     }
 }
