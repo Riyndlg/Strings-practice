a7f0674dc726774ede05d916555ddf92cd7620dc
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-04 14:06:19 +0300

    m

---
+++ Function printer error/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_printer_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
