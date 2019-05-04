50e4007d4011c1f34b3ec14f407ac18a7bb844a3
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-04 11:16:07 +0300

    c

---
+++ Number for word/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_for_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Enter number of word: ");
+            int number = int.Parse(Console.ReadLine());
+            string [] words = text.Split(' ');
+            if (number <= words.Length)
+            {
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (i == number)
+                    {
+                        Console.WriteLine(words[i].Take(1));
+                    }
+                }
+            }
+            else Console.WriteLine("There is no word for this number");
+            Console.ReadKey();
+        }
+    }
+}
