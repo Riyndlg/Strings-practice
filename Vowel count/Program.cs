df84514ee1aa62a96050c1784f45b4b82245f7ae
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-04 16:29:19 +0300

    d

---
+++ Vowel count/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowel_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
+            const string vowel = "aeiou";
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < vowel.Length; j++)
+                {
+                    if (text[i] == vowel[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Vowels: {count}");
+            Console.ReadKey();
+        }
+    }
+}
