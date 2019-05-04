49c223a30411efcb7c248878cbcfdc7e394f78bf
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-04 13:25:23 +0300

    fin

--- Capitalize words/Program.cs
+++ Capitalize words/Program.cs
@@ -12,15 +12,26 @@ namespace Capitalize_words
         {
             char letter;
             string text = "How can mirrors be real if our eyes aren't real";
-            for (int i = 0; i < text.Length; i++)
+            /*for (int i = 0; i < text.Length; i++)
             {
                 if (text[i] == ' ')
                 {
-                    letter = text[i + 1];
-                    letter = Char.ToUpper(letter);
-                    text = text.Replace(text[i + 1], letter);
+                    while (text[i + 1] == ' ')
+                    {
+                        text = text.Remove(i + 1, 1);
+                    }
                 }
+            }*/
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 1)
+                {
+                    words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1, words[i].Length - 1);
+                }
+                else words[i].ToUpper();
             }
+            text = string.Join(" ", words);
             Console.WriteLine(text);
             Console.ReadKey();
         }
